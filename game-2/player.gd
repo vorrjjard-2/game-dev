2f1e8572b5915aa60e672c47e6d3214bbe2dd4cc
PiXLw16 <axelpagsi601@gmail.com>
2026-09-16 22:45:26 +0800

    added shift to sprint

--- game-2/player.gd
+++ game-2/player.gd
@@ -12,10 +12,10 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y += JUMP_VELOCITY
 		
-	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+	if Input.is_action_just_released("ui_up") and velocity.y < 0:
 		velocity.y *= JUMP_STOP_MULTIPLIER
 		
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -23,5 +23,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if Input.is_action_pressed("sprint"):
+		velocity.x = direction * SPEED * 1.5
 
 	move_and_slide()
