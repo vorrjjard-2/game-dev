000528daabecf9b436ca4b7a062dc4e3abf44348
PiXLw16 <axelpagsi601@gmail.com>
2026-09-17 02:30:41 +0800

    added double jump, dash, and wall grabbing

--- game-2/player.gd
+++ game-2/player.gd
@@ -7,6 +7,9 @@ const SPEED := 300.0
 const JUMP_VELOCITY := -800.0
 const JUMP_STOP_MULTIPLIER := 0.2
 
+var jump_count = 0
+var can_dash = true
+
 func _ready() -> void:
 	animated_sprite.play("idle")
 
@@ -14,13 +17,19 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < 1:
 		velocity.y += JUMP_VELOCITY
+		jump_count += 1
 		
-	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
+	if Input.is_action_just_released("jump") and velocity.y < 0:
 		velocity.y *= JUMP_STOP_MULTIPLIER
+		
+	if is_on_floor():
+		if jump_count == 1:
+			can_dash = true
+		jump_count = 0
 	
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 		animated_sprite.play("move")
@@ -29,7 +38,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite.play("idle")
+	
+	if Input.is_action_pressed("sprint") and is_on_floor():
+		velocity.x = direction * SPEED * 1.75
 		
+	if Input.is_action_just_pressed("dash") and can_dash == true:
+		velocity.x += direction * SPEED * 20
+		can_dash = false
+			
 	if not is_on_floor():
 		if velocity.y < 0:
 			animated_sprite.play("jump")
@@ -40,6 +56,11 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("move")
 		else:
 			animated_sprite.play("idle")
+			
+	if is_on_wall() and not is_on_floor():
+		if Input.is_action_pressed("jump"):
+			velocity.x = direction * 0
+			velocity.y = 0
 			
 	move_and_slide()
 	
